68cd2823832415ba4bb894833858a76e5f6f74ae
kenhui521 <kenhui521@gmail.com>
2022-03-31 13:22:23 -0500

    Add Seaborn Chart as API
    Add /eda/sales_by_year API to Show Seaborn Line Chart

--- mlservice/app.py
+++ mlservice/app.py
@@ -22,6 +22,9 @@ def query(connection, query) :
     cursor.execute(query)
     return cursor.fetchall()
 
+##########
+### Flask
+##########
 app = Flask(__name__)
 
 @app.route('/')
@@ -38,6 +41,50 @@ def sales_by_year():
         """
     )
     return str(results)
+
+@app.route('/eda/sales_by_year')
+def test():
+    from flask import Flask, request, render_template, send_file
+    from matplotlib import pyplot as plt
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    import base64
+    import io
+    import pandas as pd
+    import seaborn as sns
+    
+    result_df = pd.read_sql(
+        """
+        SELECT
+            EXTRACT(year FROM "Date") AS sales_year,
+            -- EXTRACT(month FROM "Date") AS sales_month,
+            -- EXTRACT(day FROM "Date") AS sales_day,
+            SUM( CAST( REPLACE("Sale (Dollars)", '$', '') AS DECIMAL(18, 2) ) ) AS total_sales
+        FROM public.salesdata
+        GROUP BY
+            EXTRACT(year FROM "Date")
+        """,
+        db_connection
+    )
+
+    sns.set_style('dark')
+
+    x = result_df['sales_year'].tolist()
+    y = result_df['total_sales'].tolist()
+
+    fig,ax = plt.subplots(figsize=(6,6))
+    ax = sns.set(style='darkgrid')
+    sns.lineplot(
+        data=result_df,
+        x='sales_year',
+        y='total_sales',
+        marker='o'
+    )
+    canvas = FigureCanvas(fig)
+    img = io.BytesIO()
+    fig.savefig(img)
+    img.seek(0)
+    return send_file(img, mimetype='img/png')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
