e226f56feb52097c62525f7f602f1d6707d68f66
kenhui521 <kenhui521@gmail.com>
2022-03-31 08:06:37 -0500

    Ken (#5)
    * Update README.md
    
    Update TO DO
    
    * Postgres Flask API Connection
    
    - Add UI hot reloading for faster development cycles
      - Flask
    - Add Postgres Connector psycopg2
    - Expose database port 5432
    - Add sales_by_year API to return SQL results

--- mlservice/app.py
+++ mlservice/app.py
@@ -1,10 +1,43 @@
 from flask import Flask
+import psycopg2
+
+########################
+### Database Connection
+########################
+
+hostname = 'postgres'
+username = 'root'
+password = 'root'
+database = 'liquor_sales'
+
+db_connection = psycopg2.connect(
+    host=hostname,
+    user=username,
+    password=password,
+    dbname=database
+)
+
+def query(connection, query) :
+    cursor = connection.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+    return '<h1>Hello from Flask & Docker 5</h2>'
 
+@app.route('/sales_by_year')
+def sales_by_year():
+    results = query(
+        db_connection,
+        """
+            SELECT * FROM public.salesdata
+            LIMIT 10
+        """
+    )
+    return str(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
