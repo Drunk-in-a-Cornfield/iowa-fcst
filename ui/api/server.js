74fe467feb7737eedfe042958f50dc0b367ae885
Johnathan Cao <28884578+luig0@users.noreply.github.com>
2022-04-06 18:48:48 +0100

    Update UI (#14)
    * added hot reloading to ui container
    
    * rebuild package-lock.json with newer npm from container
    
    * small update to mlservice hot reloading
    
    * run ui server with nodemon
    
    * added axiosjs to ui
    
    * run flask server on all interfaces
    
    * added a comment to express server
    
    * updated features_by_store.py so it can run in container
    
    * updated k_means.py so it can run in container

---
+++ ui/api/server.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const express = require('express');
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/test-endpoint', async (req, res) => {
+  // check that mlservice is reachable
+  try {
+    const ml_res = await axios.get('http://mlservice:4000');
+    res.send(ml_res.data);
+  } catch (e) {
+    res.send(e.message);
+  }
+});
+
+app.listen(PORT, HOST);
+console.log(`Running UI server on http://${HOST}:${PORT}`);
--- ui/public/index.js
+++ ui/public/index.js
@@ -1,1 +1,10 @@
-console.log('js file successfully loaded')
+const titleDiv = document.querySelector('#title');
+titleDiv.innerHTML = 'Drunk in a Cornfield';
+
+const mainDiv = document.querySelector('#main');
+mainDiv.innerHTML = 'main content here';
+
+(async () => {
+  const res = await axios.get('/test-endpoint');
+  console.log(res.data);
+})();
