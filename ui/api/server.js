92cc270ffc87820a7116307c26d41dd7fb1bc72c
Johnathan C <28884578+luig0@users.noreply.github.com>
2022-04-06 15:00:32 +0100

    run ui server with nodemon

--- ui/api/server.js
+++ ui/api/server.js
@@ -4,8 +4,10 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 
 const app = express();
-// app.get('/', (req, res) => res.send('hello world from app'));
+
 app.use(express.static('public'));
+
+app.get('/test-endpoint', (req, res) => res.send('hello world from app!'));
 
 app.listen(PORT, HOST);
 console.log(`Running UI server on http://${HOST}:${PORT}`);
