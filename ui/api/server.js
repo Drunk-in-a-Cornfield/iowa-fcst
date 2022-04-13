e776a66d73f67c23f021dc621db3091bf90395b1
Johnathan C <28884578+luig0@users.noreply.github.com>
2022-04-13 20:43:35 +0100

    format data from flask

--- ui/api/server.js
+++ ui/api/server.js
@@ -11,7 +11,26 @@ app.use(express.static('public'));
 app.get('/cluster-data', async (req, res) => {
   try {
     const ml_res = await axios.get('http://mlservice:4000/cluster-data');
-    res.send(ml_res.data);
+    const raw = ml_res.data;
+    const keys = Object.keys(raw.pca_0);
+
+    const data = [];
+
+    for (const key of keys) {
+      data.push({
+        average_monthly_bottles_sold: raw.average_monthly_bottles_sold[key],
+        average_monthly_profit: raw.average_monthly_profit[key],
+        average_monthly_sales: raw.average_monthly_sales[key],
+        city: raw.cities[key],
+        county_no: raw.county_no[key],
+        pca_0: raw.pca_0[key],
+        pca_1: raw.pca_1[key],
+        zip_cide: raw.zip_codes[key],
+        store_no: key,
+      });
+    }
+
+    res.send(data);
   } catch (e) {
     res.send(e.message);
   }
