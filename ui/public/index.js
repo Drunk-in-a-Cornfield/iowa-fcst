7d67c895018896a8694b6c6dd1f19935343ff356
Johnathan C <28884578+luig0@users.noreply.github.com>
2022-04-13 21:39:11 +0100

    rendered circles for scatter plot

--- ui/public/index.js
+++ ui/public/index.js
@@ -6,18 +6,7 @@ const HEIGHT = 500 - MARGIN.top - MARGIN.bottom;
 // helper functions
 
 const buildAxes = (props) => {
-  const { svg, min_pca_0, max_pca_0, min_pca_1, max_pca_1 } = props;
-
-  // scale the axes
-  const xScale = d3
-    .scaleLinear()
-    .domain([min_pca_0, max_pca_0])
-    .range([MARGIN.left, WIDTH - MARGIN.right]);
-
-  const yScale = d3
-    .scaleLinear()
-    .domain([min_pca_1, max_pca_1])
-    .range([HEIGHT, 0]);
+  const { svg, xScale, yScale } = props;
 
   // create the x-axis
   const xAxisGroup = svg.append('g').attr('id', 'x-axis');
@@ -77,6 +66,19 @@ d3.json('/cluster-data').then((records) => {
       max_pca_1 = record.pca_1;
   }
 
+  // scalers
+  const xScale = d3
+    .scaleLinear()
+    .domain([min_pca_0, max_pca_0])
+    .range([MARGIN.left, WIDTH - MARGIN.right]);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([min_pca_1, max_pca_1])
+    .range([HEIGHT, 0]);
+
+  const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
   // create the svg
   const svg = d3
     .select('div#main')
@@ -87,5 +89,16 @@ d3.json('/cluster-data').then((records) => {
     .append('g')
     .attr('transform', `translate(${MARGIN.left},${MARGIN.top})`);
 
-  buildAxes({ svg, min_pca_0, max_pca_0, min_pca_1, max_pca_1 });
+  buildAxes({ svg, xScale, yScale });
+
+  svg
+    .selectAll('.dot')
+    .data(records)
+    .enter()
+    .append('circle')
+    .attr('class', 'dot')
+    .attr('r', 3.5)
+    .attr('cx', (d) => xScale(d.pca_0))
+    .attr('cy', (d) => yScale(d.pca_1))
+    .style('fill', (d) => colorScale(d.cluster));
 });
