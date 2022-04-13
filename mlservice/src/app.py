93d1f065e7bbefae5e97213fac2e339c83114fa3
Johnathan Cao <28884578+luig0@users.noreply.github.com>
2022-04-13 13:03:12 +0100

    Clusterviz (#16)
    * refactored directory structure
    
    * added script:visualize_clusters.py
    
    * renamed helpers -> modules

--- mlservice/src/app.py
+++ mlservice/src/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from os.path import exists
 import psycopg2
+import pandas as pd
 
-from features_by_store import create_feature_pickle
-from k_means import create_k_means_pickle
+from modules.features_by_store import create_feature_pickle
+from modules.k_means import create_k_means_pickle
 
 ########################
 ### Database Connection
@@ -26,6 +27,29 @@ def query(connection, query) :
     cursor.execute(query)
     return cursor.fetchall()
 
+##############################
+### Create / load pickle files
+##############################
+
+print('checking for features_by_store.pkl', flush=True)
+if not exists('./src/modules/features_by_store.pkl'):
+    print('did not detect ./src/modules/features_by_store.pkl; building now', flush=True)
+    create_feature_pickle()
+    print('done building ./src/modules/features_by_store.pkl', flush=True)
+else:
+    print('found existing feature_by_store.pkl', flush=True)
+
+print('checking for existing k_means.pkl')        
+if not exists('./src/modules/k_means.pkl'):
+    print('did not detect ./src/modules/k_means.pkl; building now', flush=True)
+    create_k_means_pickle()
+    print('done building ./src/modules/k_means.pkl', flush=True)
+else:
+    print('found existing k_means.pkl', flush=True)
+
+features_by_store_cached = pd.read_pickle('./src/modules/features_by_store.pkl')
+k_means_cached = pd.read_pickle('./src/modules/k_means.pkl')
+
 ##########
 ### Flask
 ##########
@@ -34,6 +58,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_geek():
     return '<h1>Hello from Flask & Docker 5</h2>'
+
+@app.route('/cluster-data')
+def cluster_data():
+    return 'some cluster data'
 
 @app.route('/sales_by_year')
 def sales_by_year():
@@ -90,20 +118,4 @@ def test():
     return send_file(img, mimetype='img/png')
 
 if __name__ == "__main__":
-    print('checking for features_by_store.pkl', flush=True)
-    if not exists('./features_by_store.pkl'):
-        print('did not detect ./features_by_store.pkl; building now', flush=True)
-        create_feature_pickle()
-        print('done building ./features_by_store.pkl', flush=True)
-    else:
-        print('found existing feature_by_store.pkl', flush=True)
-
-    print('checking for existing k_means.pkl')        
-    if not exists('./k_means.pkl'):
-        print('did not detect ./k_means.pkl; building now', flush=True)
-        create_k_means_pickle()
-        print('done building ./k_means.pkl', flush=True)
-    else:
-        print('found existing k_means.pkl', flush=True)
-    
     app.run(host='0.0.0.0', port=4000, debug=True)
---
+++ mlservice/src/scripts/visualize_clusters.py
@@ -0,0 +1,54 @@
+# https://www.askpython.com/python/examples/plot-k-means-clusters-python
+
+#Importing required modules
+
+# from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_pickle('../modules/features_by_store.pkl')
+
+ignore_columns = ['average_monthly_sales', 'average_monthly_profit']
+
+X_columns = [i for i in df.columns.tolist() if i not in ignore_columns]
+X = df[X_columns]
+
+# Standardize the data to have a mean of ~0 and a variance of 1
+X_std = StandardScaler().fit_transform(X)
+
+# Create a PCA instance: pca
+pca = PCA(n_components=2)
+principalComponents = pca.fit_transform(X_std)
+
+#Initialize the class object
+kmeans = KMeans(n_clusters=3,
+                init="k-means++",
+                n_init=10,
+                tol=1e-04,
+                random_state=59)
+
+#predict the labels of clusters.
+label = kmeans.fit_predict(principalComponents)
+
+#Getting unique labels
+u_labels = np.unique(label)
+
+print('label:', label)
+print('u_labels:', u_labels)
+print('X:', X)
+print(f'principalComponents ({principalComponents.shape}):\n', principalComponents)
+
+# plotting the results:
+for i in u_labels:
+    plt.scatter(principalComponents[label == i, 0], principalComponents[label == i, 1], label=i)
+plt.legend()
+
+ax = plt.gca()
+ax.set_ylim([-5, 20])
+
+plt.savefig('04-clusters.png')
