36eaec7de0a1c03aa234e7950a626aa752e1461a
unknown <edin.3@osu.edu>
2022-04-22 21:29:49 -0400

    Add RNN predictor

--- mlservice/src/scripts/experiments.py
+++ mlservice/src/scripts/experiments.py
@@ -1,17 +1,25 @@
 import pandas as pd
 import numpy as np
 import datetime as dt
+import random
 
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelBinarizer
 
+from keras.models import Sequential
+from keras.layers import SimpleRNN, Dense
+
+counties = ['ADAIR', 'ADAMS', 'ALLAMAKEE', 'APPANOOSE', 'AUDUBON', 'BENTON', 'BLACK HAWK', 'BOONE', 'BREMER', 'BUCHANAN', 'BUENA VIST', 'BUENA VISTA', 'BUTLER', 'CALHOUN', 'CARROLL', 'CASS', 'CEDAR', 'CERRO GORD', 'CERRO GORDO', 'CHEROKEE', 'CHICKASAW', 'CLARKE', 'CLAY', 'CLAYTON', 'CLINTON', 'CRAWFORD', 'DALLAS', 'DAVIS', 'DECATUR', 'DELAWARE', 'DES MOINES', 'DICKINSON', 'DUBUQUE', 'EMMET', 'FAYETTE', 'FLOYD', 'FRANKLIN', 'FREMONT', 'GREENE', 'GRUNDY', 'GUTHRIE', 'HAMILTON', 'HANCOCK', 'HARDIN', 'HARRISON', 'HENRY', 'HOWARD', 'HUMBOLDT', 'IDA', 'IOWA', 'JACKSON', 'JASPER', 'JEFFERSON', 'JOHNSON', 'JONES', 'KEOKUK', 'KOSSUTH', 'LEE', 'LINN', 'LOUISA', 'LUCAS', 'LYON', 'MADISON', 'MAHASKA', 'MARION', 'MARSHALL', 'MILLS', 'MITCHELL', 'MONONA', 'MONROE', 'MONTGOMERY', 'MUSCATINE', "O'BRIEN", 'OBRIEN', 'OSCEOLA', 'PAGE', 'PALO ALTO', 'PLYMOUTH', 'POCAHONTAS', 'POLK', 'POTTAWATTA', 'POTTAWATTAMIE', 'POWESHIEK', 'RINGGOLD', 'SAC', 'SCOTT', 'SHELBY', 'SIOUX', 'STORY', 'TAMA', 'TAYLOR', 'UNION', 'VAN BUREN', 'WAPELLO', 'WARREN', 'WASHINGTON', 'WAYNE', 'WEBSTER', 'WINNEBAGO', 'WINNESHIEK', 'WOODBURY', 'WORTH', 'WRIGHT']
+counties_encoding = {county: num for num, county in enumerate(counties) }
 """
 Do some value conversions
 """
 def transform_columns(df):
 	df["State Bottle Cost"] = df["State Bottle Cost"].str.replace("$", "").astype(float)
 	df["State Bottle Retail"] = df["State Bottle Retail"].str.replace("$", "").astype(float)
-	df["Sale (Dollars)"] = df["Sale (Dollars)"].str.replace("$", "").astype(float)
+	df["tx_amt"] = df["Sale (Dollars)"].str.replace("$", "").astype(float)
+	df["County"] = df["County"].str.upper()
 
 	return df
 """
@@ -75,16 +83,52 @@ def plot_state_sales_per_week(df):
 	plt.show()
 
 
+def deep_learning_regression_by_county(df):
+	time_steps = 6
+	weekly_sales_by_county = df.groupby([df.Date, df.County])['tx_amt'].sum()
+
+	samples = []
+	# Create a ton of samples of n step series, we'll shuffle them later so no harm in some overlap between the series'
+	for c in counties:
+		county_level_stats = weekly_sales_by_county[weekly_sales_by_county.index.isin([c], level=1)]
+		for i in range(len(county_level_stats) - time_steps):
+			samples.append(county_level_stats[range(i, i + time_steps)])
+	random.shuffle(samples)
+
+	y_samples = []
+	x_samples = []
+	for sample in samples:
+		y_samples.append(sample[-1])
+		sample_x_features = []
+		for i in range(len(sample)):
+			feature_vec = np.zeros((len(counties) + 2))
+			feature_vec[0] = sample.index[i][0].toordinal()
+			feature_vec[1] = sample.index[i][0].weekday()
+			feature_vec[2 + counties_encoding[sample.index[i][1]]] = 1
+			sample_x_features.append(feature_vec)
+		x_samples.append(sample_x_features)
+
+	y_samples = np.array(y_samples)
+	x_samples = np.array(x_samples)
+
+
+	model = Sequential()
+	model.add(SimpleRNN(1, input_shape=(6,105), activation ='linear'))
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	model.fit(x_samples, y_samples, batch_size=1024, epochs=1000)
+
+	print(model.predict(np.array([x_samples[103]])))
+
 if __name__ == "__main__":
 	print("hi")
 	pd.set_option('display.max_colwidth', None)
 	pd.set_option("display.max_columns", None)
 
-	data = pd.read_csv("data/Iowa_Liquor_Sales.csv", parse_dates=[1])
+	data = pd.read_csv("../../data/Iowa_Liquor_Sales.csv", parse_dates=[1])
 	data = data.sort_values(by=['Date'])
 	data = transform_columns(data)
 
 	#plot_state_sales_per_day(data)
 	#plot_polk_county_sales(data)
-	predict_polk_county_sales_per_week_linear(data)
+	deep_learning_regression_by_county(data)
 
