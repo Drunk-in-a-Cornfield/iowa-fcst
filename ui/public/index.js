845152a6a3013c00617ca1c5adf3cf25ba3ce7c3
Johnathan C <28884578+luig0@users.noreply.github.com>
2022-04-24 19:03:38 +0100

    styled chart switcher buttons

--- ui/public/index.js
+++ ui/public/index.js
@@ -4,13 +4,23 @@ const chartswitcherDiv = document.querySelector('div#chartswitcher');
 
 const forecastButton = document.createElement('button');
 forecastButton.id = 'btn-forecast';
-forecastButton.innerHTML = 'Load Forecast Chart';
-forecastButton.onclick = useForecast;
+forecastButton.className = 'btn-switcher-clicked';
+forecastButton.innerHTML = 'ML Forecast Chart';
+forecastButton.onclick = () => {
+  forecastButton.className = 'btn-switcher-clicked';
+  scatterplotButton.className = 'btn-switcher';
+  useForecast();
+};
 
 const scatterplotButton = document.createElement('button');
 scatterplotButton.id = 'btn-scatterplot';
-scatterplotButton.innerHTML = 'Load Scatterplot';
-scatterplotButton.onclick = useScatterPlot;
+scatterplotButton.className = 'btn-switcher';
+scatterplotButton.innerHTML = 'K-Means Scatterplot';
+scatterplotButton.onclick = () => {
+  scatterplotButton.className = 'btn-switcher-clicked';
+  forecastButton.className = 'btn-switcher';
+  useScatterPlot();
+};
 
 chartswitcherDiv.appendChild(forecastButton);
 chartswitcherDiv.appendChild(scatterplotButton);
